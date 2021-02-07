63b7dd120b2b788ff0a39f91e39a7b973eda1294
Rohit <48995037+rohit-vrrr@users.noreply.github.com>
2021-02-07 17:34:02 +0530

    Added sounds and color recognition

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-alert("Welcome!");
+var gamePattern = [];
+
+var buttonColors = ["yellow", "blue", "red", "green"];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+}
